a318dd5147c7320bf43fb4046c702b6b0cff1bff
Chris Keller <chris@commercialprogression.com>
2016-05-01 10:32:03 -0400

    Added waterskin recipe

--- src/main/java/net/tomgeekery/leatherworking/crafting/LWCrafting.java
+++ src/main/java/net/tomgeekery/leatherworking/crafting/LWCrafting.java
@@ -14,6 +14,9 @@ public class LWCrafting {
 		
 		// Saddle
 		GameRegistry.addRecipe(new ItemStack(Items.saddle),	"lll", " s ", " i ", 'l', Items.leather, 's', LWItems.leatherStrip,	'i', Items.iron_ingot);
+		
+		// Waterskin
+		GameRegistry.addRecipe(new ItemStack(LWItems.waterSkin), " l ", "l l", " l ", 'l', Items.leather);
 				
 	}
 	
