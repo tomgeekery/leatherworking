4170ff168a86395500e5e12113bc26cd969b77fe
Chris Keller <chris@commercialprogression.com>
2016-05-15 08:40:08 -0400

    Added leather from cork recipe

--- src/main/java/net/tomgeekery/leatherworking/crafting/LWCrafting.java
+++ src/main/java/net/tomgeekery/leatherworking/crafting/LWCrafting.java
@@ -9,13 +9,23 @@ public class LWCrafting {
 
 	public static void LWCraftingInit() {
 		
+		// Leather from cork
+		GameRegistry.addShapelessRecipe(new ItemStack(Items.leather), 
+				new ItemStack(LWItems.cork),
+				new ItemStack(LWItems.cork),
+				new ItemStack(LWItems.cork),
+				new ItemStack(LWItems.cork),
+				new ItemStack(LWItems.cork),
+				new ItemStack(LWItems.cork)
+				);
+		
 		// Leather Strip
 		GameRegistry.addRecipe(new ItemStack(LWItems.leatherStrip, 3), "l", 'l', Items.leather);
 		
 		// Saddle
 		GameRegistry.addRecipe(new ItemStack(Items.saddle),	"lll", " s ", " i ", 'l', Items.leather, 's', LWItems.leatherStrip,	'i', Items.iron_ingot);
 		
-		// Waterskin
+		// Water skin
 		GameRegistry.addRecipe(new ItemStack(LWItems.waterSkin), " l ", "l l", " l ", 'l', Items.leather);
 				
 	}
