e19e83d35a4ec3ba37aa6c814638a8c9d507192c
Chris Keller <ckeller.tomgeekery@gmail.com>
2018-02-18 12:29:58 -0500

    Added static instance class to main class; added preinit, init, and post init handlers

--- src/main/java/com/teamgeekery/leatherworking/Leatherworking.java
+++ src/main/java/com/teamgeekery/leatherworking/Leatherworking.java
@@ -2,6 +2,10 @@
 package com.teamgeekery.leatherworking;
 
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = Leatherworking.MODID, name = Leatherworking.NAME, version = Leatherworking.VERSION)
 public class Leatherworking
@@ -9,4 +13,19 @@ public class Leatherworking
     public static final String MODID = "leatherworking";
     public static final String NAME = "Leatherworking";
     public static final String VERSION = "1.0";
+    
+    @Mod.Instance(MODID)
+    public static Leatherworking instance;
+    
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {}
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event) 
+    {
+    	System.out.println(NAME + " is loading!");
+    }
+    
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) {}   
 }
